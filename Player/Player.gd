37cf9626b83c4b36ef191b687b642146104702a4
PrinceCaspian <iniyanijoseph@gmail.com>
2023-02-01 19:50:53 -0600

    Merge Player-Action With Main & Resolve Conflicts

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,6 +1,7 @@
 extends KinematicBody2D
 
 signal action_complete
+onready var floorFindRayCast : RayCast2D = $"%FloorFindRayCast"
 
 func _ready():
 	# Set Up Signal Bus
@@ -9,7 +10,13 @@ func _ready():
 	PlayerActionSignalBus.setup()
 
 func slam():
-	#Replace With Code to Slam
+	var localFloor = floorFindRayCast.get_collision_point()
+	print(localFloor)
+	var tween = get_tree().create_tween()
+	tween.tween_property(self, "position:y", localFloor.y - floorFindRayCast.position.y, 1).\
+	set_trans(Tween.TRANS_QUINT).\
+	set_ease(Tween.EASE_IN)
+	yield(tween, "finished")
 	action_complete()
 
 func gravitate():
@@ -23,3 +30,6 @@ func repel():
 # Call Whenever an action is complete to update player state
 func action_complete():
 	emit_signal("action_complete")
+
+func _physics_process(delta):
+	move_and_slide(Vector2(200, 0))
